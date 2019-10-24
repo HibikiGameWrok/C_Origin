47a1ffcd65e451e06e75efdc2a46165e47f5f82d
hibiki414 <gameword.hibiki@gmail.com>
2019-10-24 19:57:04 +0900

    Soureファイルを追加

---
+++ C_Origin/Source/C_Origin/C_Origin.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "C_Origin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, C_Origin, "C_Origin" );
---
+++ C_Origin/Source/C_Origin/C_OriginGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "C_OriginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class C_ORIGIN_API AC_OriginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
